532ce42411d10898b3de55a590a0af89a5d88039
Ben Hauser <ben@hauser.id>
2020-06-15 20:49:31 +0400

    test: add fixtures for pool mock and airdrop token

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import pytest
+from brownie import compile_source
+
+POOL_MOCK = """
+from vyper.interfaces import ERC20
+
+@public
+def claim_airdrop(addr: address, value: uint256):
+    ERC20(addr).transfer(msg.sender, value)
+    """
 
 
 @pytest.fixture(autouse=True)
@@ -14,3 +23,14 @@ def lp_token(ERC20_Revert, accounts):
 @pytest.fixture(scope="module")
 def escrow(AirdropEscrow, accounts, lp_token):
     yield AirdropEscrow.deploy("Airdrop Escrow", "AIR", lp_token, {'from': accounts[0]})
+
+
+@pytest.fixture(scope="module")
+def pool(accounts):
+    deployer = compile_source(POOL_MOCK).Vyper
+    yield deployer.deploy({'from': accounts[0]})
+
+
+@pytest.fixture(scope="module")
+def airdrop_token(ERC20_Revert, accounts):
+    yield ERC20_Revert.deploy("Airdrop One", "AD1", 18, {'from': accounts[0]})
