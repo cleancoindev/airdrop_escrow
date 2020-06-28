0329221bec56a0e0a02f6f61457c6d5e21e78508
Ben Hauser <ben@hauser.id>
2020-06-28 16:26:48 +0400

    test: unit tests for claim

--- tests/unitary/test_claim.py
+++ tests/unitary/test_claim.py
@@ -1,3 +1,4 @@
+import brownie
 import pytest
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
@@ -15,9 +16,11 @@ def test_claim(accounts, escrow, airdrop_token, rpc):
     escrow.add_token(airdrop_token, {'from': accounts[0]})
     airdrop_token.transfer(escrow, 10**18, {'from': accounts[0]})
 
-    rpc.sleep(1)  # Need to wait at least one second between epochs (otherwise funds register in the next epoch)
+    # Need to wait at least one second between epochs
+    # (otherwise funds register in the next epoch)
+    rpc.sleep(1)
 
-    escrow.checkpoint()  # Now escrow is aware of tokens
+    escrow.checkpoint()
     escrow.claim(airdrop_token, {'from': accounts[0]})
 
     assert airdrop_token.balanceOf(accounts[0]) == 10**18
@@ -34,3 +37,48 @@ def test_claim_existing_balance(accounts, escrow, airdrop_token, rpc):
     escrow.claim(airdrop_token, {'from': accounts[0]})
 
     assert airdrop_token.balanceOf(accounts[0]) == 10**18
+
+
+def test_claim_no_balance(accounts, escrow, airdrop_token):
+    airdrop_token.transfer(escrow, 10**18, {'from': accounts[0]})
+    escrow.add_token(airdrop_token, {'from': accounts[0]})
+
+    with brownie.reverts("User must have some deposits"):
+        escrow.claim(airdrop_token, {'from': accounts[1]})
+
+
+def test_claim_no_airdrop(accounts, escrow, airdrop_token):
+    airdrop_token.transfer(escrow, 10**18, {'from': accounts[0]})
+
+    with brownie.reverts("Airdrops in this token are not yet received"):
+        escrow.claim(airdrop_token, {'from': accounts[0]})
+
+
+def test_account_has_no_balance_at_airdrop(accounts, rpc, escrow, airdrop_token, lp_token):
+    airdrop_token.transfer(escrow, 10**18, {'from': accounts[0]})
+
+    lp_token._mint_for_testing(10**24, {'from': accounts[1]})
+    lp_token.approve(escrow, 10**24, {'from': accounts[1]})
+    escrow.deposit(10**24, {'from': accounts[1]})
+
+    escrow.withdraw(10**24, {'from': accounts[0]})
+    rpc.sleep(1)
+    escrow.add_token(airdrop_token, {'from': accounts[0]})
+    rpc.sleep(1)
+
+    escrow.claim(airdrop_token, {'from': accounts[0]})
+
+    assert airdrop_token.balanceOf(accounts[0]) == 0
+
+
+def test_escrow_is_empty_at_airdrop(accounts, rpc, escrow, airdrop_token, lp_token):
+    airdrop_token.transfer(escrow, 10**18, {'from': accounts[0]})
+
+    escrow.withdraw(10**24, {'from': accounts[0]})
+    rpc.sleep(1)
+    escrow.add_token(airdrop_token, {'from': accounts[0]})
+    rpc.sleep(1)
+
+    escrow.claim(airdrop_token, {'from': accounts[0]})
+
+    assert airdrop_token.balanceOf(accounts[0]) == 0
